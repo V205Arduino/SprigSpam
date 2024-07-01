c629c6377b00ceb60840b6a8826428b61b16d602
V205 <138826219+V205Arduino@users.noreply.github.com>
2024-07-01 12:47:37 -0700

    this is a no delay mess

--- Arduino/sprigSpamArduino/sprigSpamArduino.ino
+++ Arduino/sprigSpamArduino/sprigSpamArduino.ino
@@ -29,7 +29,7 @@ TODO: get
 
 Arduino_DataBus *bus = new Arduino_HWSPI(22, 21);
 //Arduino_GFX *gfx = new Arduino_ILI9341(bus, 17 /* RST */);
-Arduino_GFX *gfx = new Arduino_ST7735(bus,26, 90);
+Arduino_GFX *gfx = new Arduino_ST7735(bus, 26, 3);
 /*
 
 Arduino_ST7735::Arduino_ST7735(
@@ -44,10 +44,21 @@ const uint8_t adv_data[] = {
 const uint8_t adv_data2[] = {
   0x1e, 0xff, 0x4c, 0x00, 0x07, 0x19, 0x07, 0x13, 0x20, 0x75, 0xaa, 0x30, 0x01, 0x00, 0x00, 0x45, 0x12, 0x12, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
-void setup() 
-{
+
+
+// Define pin numbers for the buttons
+const int wButtonPin = 5;
+const int aButtonPin = 6;
+const int sButtonPin = 7;
+const int dButtonPin = 8;
+const int iButtonPin = 12;
+const int jButtonPin = 13;
+const int kButtonPin = 14;
+const int lButtonPin = 15;
+
+void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); // begin serial comunication.
+  Serial.begin(115200);  // begin serial comunication.
   delay(1000);
   Serial.println(F("STARTING " __FILE__ " FROM " __DATE__ __TIME__));
   BTstack.setup();
@@ -55,26 +66,45 @@ void setup()
   //BTstack.setAdvData(sizeof(adv_data), adv_data);
   //BTstack.startAdvertising();
 
-   // put your setup code here, to run once:
-  Serial.begin(115200); // begin serial comunication.
+  pinMode(wButtonPin, INPUT_PULLUP);
+  pinMode(aButtonPin, INPUT_PULLUP);
+  pinMode(sButtonPin, INPUT_PULLUP);
+  pinMode(dButtonPin, INPUT_PULLUP);
+  pinMode(iButtonPin, INPUT_PULLUP);
+  pinMode(jButtonPin, INPUT_PULLUP);
+  pinMode(kButtonPin, INPUT_PULLUP);
+  pinMode(lButtonPin, INPUT_PULLUP);
   pinMode(17, OUTPUT);
-  digitalWrite(17,HIGH);
+  digitalWrite(17, HIGH);
   delay(1000);
-  Serial.println(F("STARTING " __FILE__ " from " __DATE__ __TIME__));
   gfx->begin();
-gfx->fillScreen(BLACK);
-gfx->setCursor(10, 10);
-gfx->setTextColor(RED);
+  gfx->fillScreen(BLACK);
+  gfx->setCursor(10, 10);
+  gfx->setTextColor(GREEN);
 
-gfx->println("Hello World!");
-gfx->setTextColor(BLUE);
+  gfx->println("Press S to start");
+  gfx->println("Press S to start");
+  const unsigned long previousTime = 0;
+  const long interval = 1000;
+  int dotCounter = 0;
+  while (digitalRead(sButtonPin) == LOW) {
 
+    unsigned long currentTime = millis();
+    if (currentTime - previousTime >= interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
 
+      if (1) {
+
+      } else {
+      }
+    }
+    gfx->fillScreen(BLACK);
+  }
 }
 
-void loop() 
-{
-  
+void loop() {
+
   /*
   BTstack.stopAdvertising();
   delay(1000);
@@ -91,8 +121,5 @@ void loop()
   // put your main code here, to run repeatedly:
    BTstack.loop();
    */
-   delay(1000);
-  
-
+  delay(1000);
 }
-
