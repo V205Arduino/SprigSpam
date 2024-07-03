83ab5ab28e8c5fd0db82448325b3e8e42826c0b0
V205 <138826219+V205Arduino@users.noreply.github.com>
2024-07-03 12:02:35 -0700

    uh what am I doing

--- Arduino/sprigSpamArduino/sprigSpamArduino.ino
+++ Arduino/sprigSpamArduino/sprigSpamArduino.ino
@@ -1,1 +1,22 @@
+/*
+  TITLE
+  By V205
+*/
+
+
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200); // begin serial comunication.
+  delay(1000);
+  Serial.println(F("STARTING " __FILE__ " from " __DATE__ __TIME__));
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+}
 
